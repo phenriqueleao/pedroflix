8ad54e5e507422bb4019edfcc354e7046ce0052a
Pedro <phenriqueleao@gmail.com>
2020-07-30 01:34:19 -0300

    Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import Logo from "../../assets/img/Logo.png";
 import "./Menu.css";
 import Button from '../Button';
@@ -11,7 +12,7 @@ function Menu() {
         <img className="Logo" src={Logo} alt="PedroFlix logo" />
       </a>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
       </Button>
     </nav>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import dadosIniciais from './data/dados_iniciais.json';
-import Menu from './components/Menu';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import Menu from '../../components/Menu';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414" }} >
       <Menu />
@@ -43,4 +43,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
